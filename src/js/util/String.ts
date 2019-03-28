bf1e0e96b3a3a31745a996ed89dd4727f0ee2996
Stephen Poole <stephenwpoole@gmail.com>
2019-03-28 16:30:58 -0400

    Fix build error. Remove secret keys from travis.yml. Invalidated old keys.

--- src/js/util/String.ts
+++ src/js/util/String.ts
@@ -1,7 +1,7 @@
 export class StringClass {
     public static random(length: number = 1, prefix: string = ''): string {
         let result = prefix;
-        let size = length < 1 ? 1 : length;
+        const size = length < 1 ? 1 : length;
 
         result += Math.random()
             .toString(36)
